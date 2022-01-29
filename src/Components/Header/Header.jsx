008c24f02974a76e1f0836882138674707b40da4
abdurashiidov <abdurashiidov18@gmail.com>
2022-01-29 23:31:32 +0300

    add

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,13 +9,14 @@ function Header() {
       <header>
         {/* Header */}
         <img src={logo} alt="logo" width={150} height={150} />
-        <img src={logo2} alt="logo" width={300} height={150} />
+        {/* <img src={logo2} alt="logo" width={300} height={150} /> */}
 
         <ul className="header-list">
-          <li className="header-item">Bosh sahifa</li>
-          <li className="header-item">Biz haqimizda</li>
-          <li className="header-item">Buyurtma</li>
+          <li className="header-item"><a href="#"> Bosh sahifa </a> </li>
+          <li className="header-item"> <a href="#"> Biz haqimizda </a> </li>
+          <li className="header-item"> <a href="#"> Buyurtma qilish </a> </li>
         </ul>
+
       </header>
 
     </div>
