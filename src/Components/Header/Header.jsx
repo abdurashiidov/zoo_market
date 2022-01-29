551ca5ac0c41c7e274bd7da1c9e9270d29f00169
abdurashiidov <abdurashiidov18@gmail.com>
2022-01-29 23:12:41 +0300

    commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./Header.scss"
+import logo from "../../img/logo-exam.png"
+import logo2 from "../../img/logo.jpg"
+
+function Header() {
+  return (
+    <div>
+      <header>
+        {/* Header */}
+        <img src={logo} alt="logo" width={150} height={150} />
+        <img src={logo2} alt="logo" width={300} height={150} />
+
+        <ul className="header-list">
+          <li className="header-item">Bosh sahifa</li>
+          <li className="header-item">Biz haqimizda</li>
+        </ul>
+      </header>
+
+    </div>
+  );
+}
+
+export default Header;
